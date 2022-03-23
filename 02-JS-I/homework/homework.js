398a9f83dd24ae76e707f3a6b4bed5a535c9891f
Gimena90 <gime_union_11@hotmail.com>
2022-03-23 19:21:29 -0300

    first commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -211,7 +211,7 @@ function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  return 'Hola' + nombre + '!';
+  return 'Hola' + Martin + '!';
   
 }
 
